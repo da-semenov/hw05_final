e7015b01c2faada0deb582f237c9afa361118758
da-semenov <da_semenov@mail.ru>
2021-01-12 03:31:48 +0300

    Static and media

--- yatube/settings.py
+++ yatube/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = [
         "127.0.0.1",
         "[::1]",
         "testserver",
+        "*",
 ]
 
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -152,3 +155,7 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- yatube/urls.py
+++ yatube/urls.py
@@ -29,6 +29,10 @@ urlpatterns += [
 
 
 if settings.DEBUG:
+    import debug_toolbar
+    
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
+
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
